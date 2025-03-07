3ab9951a68ae9c11dec88120bc11dcf97aa9b775
Premlakshay Godara <89251510+Premlak@users.noreply.github.com>
2025-03-07 13:56:16 +0530

    Delete app directory

--- app/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { DashboardProvider } from "../_components/AdminContext";
-export default function Layout({children}: any){
-    return (
-        <DashboardProvider>
-            {children}
-        </DashboardProvider>
-    )
-}
--- app/subAdmin/console/alter/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-const AlterContent = dynamic(() => import("../../../_components/justTring"), { ssr: false });
-export default function AlterPage() {
-  return <AlterContent />;
-}
