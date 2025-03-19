c0246ebb64a4c99912f9dabef469c1a182e31995
Premlakshay Godara <89251510+Premlak@users.noreply.github.com>
2025-03-19 17:48:51 +0530

    Update page.tsx

--- app/courses/[id]/page.tsx
+++ app/courses/[id]/page.tsx
@@ -59,7 +59,7 @@ export default function Home({ params }: { params: { id: String } }) {
     });
     const data = await res.json();
     setCont(
-      `<style>body, * {background-color: ${uTheme === "dark" ? "#000000" : "#FFFFFF"} !important; background: ${uTheme === "dark" ? "#000000" : "#FFFFFF"} !important; ::-webkit-scrollbar {width: 0;}</style><div style='margin-bottom: 50px !important;'>${data.content[0]?.con.replace(/<p[^>]*>\s*Powered by\s*<a[^>]*Froala Editor[^>]*>.*?<\/a>\s*<\/p>/g, '')} </div><script>document.addEventListener('selectstart', (e) => e.preventDefault());document.addEventListener('mousedown', (e) => e.preventDefault()); document.body.style.backgroundColor = ${uTheme === "dark" ? "#000000" : "#FFFFFF"};</script>`
+      `<style>body, * {background-color: ${uTheme === "dark" ? "#000000" : "#FFFFFF"} !important; background: ${uTheme === "dark" ? "#000000" : "#FFFFFF"} !important; ::-webkit-scrollbar {width: 0;}</style><div style='margin-bottom: 50px !important; display: flex !important; flex-direction: column !important; align-items: center !important;'>${data.content[0]?.con.replace(/<p[^>]*>\s*Powered by\s*<a[^>]*Froala Editor[^>]*>.*?<\/a>\s*<\/p>/g, '')} </div><script>document.addEventListener('selectstart', (e) => e.preventDefault());document.addEventListener('mousedown', (e) => e.preventDefault()); document.body.style.backgroundColor = ${uTheme === "dark" ? "#000000" : "#FFFFFF"};</script>`
     );
   };
   React.useEffect(() => {
